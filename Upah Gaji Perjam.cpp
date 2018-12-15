fb95b87c0d3f3a2d1bbfea56589c382c396b3c6b
ItaLestari11 <45660967+ItaLestari11@users.noreply.github.com>
2018-12-15 15:57:05 +0700

    Add files via upload

---
+++ Upah Gaji Perjam.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+int jamkerja,hasil;
+
+cout<<"Menghitung Upah Perjam"<<endl;
+cout<<"ITA LESTARI"<<endl;
+cout<<"311810183"<<endl;
+cout<<"masukan jumlah jam kerja"<<endl;
+cin>>jamkerja;
+
+hasil=jamkerja*5000;
+cout<<"maka upah karyawan adalah"<<hasil;
+return 0;
+}
